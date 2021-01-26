50fbc5cceef6da0632b22b92f362945c02de402e
Gabriel-Aragao <gabriellamparina@gmail.com>
2021-01-26 02:54:49 -0300

    initial commit

---
+++ src/test/java/br/edu/ifpb/dac/gabriel/aragao/bookapi/BookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.dac.gabriel.aragao.bookapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
